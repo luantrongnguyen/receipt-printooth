d6e49c94e4ddb4a5fe935bbe0237d63a2ff65621
LuanNguyenTrong <trongluan35895342.fc@gmail.com>
2024-10-02 17:55:39 +0700

    no message

--- printer_bluetooth/build.gradle.kts
+++ printer_bluetooth/build.gradle.kts
@@ -8,7 +8,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.luantrongnguyen"
             artifactId = "receipt-printooth"
-            version = "1.0.6-release"
+            version = "1.0.7-release"
 
             // Use the releaseAar component for Android library
             artifact("$buildDir/outputs/aar/${project.name}-release.aar")
