226c0e54f49d51fd25b1e6c837ea7fb1f6d671fd
LuanNguyenTrong <trongluan35895342.fc@gmail.com>
2024-10-03 12:11:06 +0700

    no message

--- printer_bluetooth/build.gradle.kts
+++ printer_bluetooth/build.gradle.kts
@@ -8,11 +8,45 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.luantrongnguyen"
             artifactId = "receipt-printooth"
-            version = "1.0.5-release"
+            version = "1.1.0-release"
 
             // Use the releaseAar component for Android library
             artifact("$buildDir/outputs/aar/${project.name}-release.aar")
-
+//            from(components["java"])
+            pom {
+                withXml {
+                    asNode().appendNode("dependencies").apply {
+                        appendNode("dependency").apply {
+                            appendNode("groupId", "io.github.pilgr")
+                            appendNode("artifactId", "paperdb")
+                            appendNode("version", "2.7.2")
+                        }
+                        appendNode("dependency").apply {
+                            appendNode("groupId", "com.afollestad.assent")
+                            appendNode("artifactId", "core")
+                            appendNode("version", "3.0.2")
+                        }
+                        // Dependency 3: androidx.appcompat
+                        appendNode("dependency").apply {
+                            appendNode("groupId", "androidx.appcompat")
+                            appendNode("artifactId", "appcompat")
+                            appendNode("version", "1.6.1")
+                        }
+                        // Dependency 4: google material
+                        appendNode("dependency").apply {
+                            appendNode("groupId", "com.google.android.material")
+                            appendNode("artifactId", "material")
+                            appendNode("version", "1.9.0")
+                        }
+                        // Dependency 5: androidx.swiperefreshlayout
+                        appendNode("dependency").apply {
+                            appendNode("groupId", "androidx.swiperefreshlayout")
+                            appendNode("artifactId", "swiperefreshlayout")
+                            appendNode("version", "1.1.0")
+                        }
+                    }
+                }
+            }
         }
     }
 }
